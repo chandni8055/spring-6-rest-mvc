043eeb77eb121566e50b654519f7cf014be7dfc4
chandni8055 <95699966+chandni8055@users.noreply.github.com>
2023-06-29 17:58:31 +0530

    GET,POST,PUT,DELETE,PATCH

--- src/main/java/com/infy/spring6restmvc/service/BeerService.java
+++ src/main/java/com/infy/spring6restmvc/service/BeerService.java
@@ -2,8 +2,19 @@ package com.infy.spring6restmvc.service;
 
 import com.infy.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBeers();
+
     Beer getBeerById(UUID id);
+
+    Beer savedNewBeer(Beer beer);
+
+    void updateBeerById(UUID beerId, Beer beer);
+
+    void deleteById(UUID beerId);
+
+    void patchBeerById(UUID beerId, Beer beer);
 }
---
+++ src/main/java/com/infy/spring6restmvc/service/CustomerService.java
@@ -0,0 +1,20 @@
+package com.infy.spring6restmvc.service;
+
+import com.infy.spring6restmvc.model.Customer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CustomerService {
+    Customer getCustomerById(UUID uuid);
+
+    List<Customer>getAllCustomers();
+
+    Customer newSavedCustomer(Customer customer);
+
+    void updateCustomerById(UUID customerId, Customer customer);
+
+    void deleteCustomerById(UUID customerId);
+
+    void patchCustomerById(UUID customerId, Customer customer);
+}
